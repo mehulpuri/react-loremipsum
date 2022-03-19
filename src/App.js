c4bef21e5b450d863fe4061c3bccd4fd37bff395
mehulpuri <lonewolfbs1@gmail.com>
2022-03-19 20:41:03 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import data from './data';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (event) => {
+    
+    var amount = parseInt(count);
+    if(count <= 0){
+      amount = 1;
+    }
+    setText(data.slice(0, amount));
+    event.preventDefault();
+  }
+  const handleChange = (event) => {
+    setCount(event.target.value);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section className="section-center">
+      <h3>Generate Fun Lorem Ipsum</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">Paragraphs: </label>
+        <input type="number" name="amount" id="amount" value={count} onChange={handleChange} />
+        <button type="submit" className="btn">Generate</button>
+      </form>
+      <article className="lorem-text">
+
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>
+        })}
+
+      </article>
+
+    </section>
+  )
 }
 
 export default App;
---
+++ src/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Footer(){
+    return <footer className="lorem-footer">made with 💖 by mehulpuri</footer>
+}
+
+export default Footer;
